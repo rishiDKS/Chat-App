902e1d0354dc6869f5851ec80b179307a426a6b8
rishiDKS <dksrishi007@gmail.com>
2026-08-13 18:00:35 +0530

    Message routes refractored

--- backend/src/controller/message.controller.js
+++ backend/src/controller/message.controller.js
@@ -44,7 +44,12 @@ export const sendMessage = async (req, res) => {
         if (!text && !image) {
             return res.status(400).json({ message: "Message text or image is required" });
         }
-
+        if(senderId.equals(receiverId)){
+            return res.status(400).json({message:"Cannot send messages to yourself"});
+        }
+        const receiverExists=await User.exists({_id:receiverId});
+        if(!receiverExists)return res.status(400).json({message:"Receiver not found."});
+        
         let imageUrl = "";
         if (image) {
             //upload base64 image to cloudinary
--- backend/src/model/Message.js
+++ backend/src/model/Message.js
@@ -11,7 +11,7 @@ const messageSchema = new mongoose.Schema({
         required:true
     },text:{
         type:String,
-        required:true,
+        trim:true,
         maxlength:2000
     },
     image:{
